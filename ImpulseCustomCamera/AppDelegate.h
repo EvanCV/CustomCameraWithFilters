0fbfd67247b2d256cf3962d6f0571f806b4870a6
EvanCV <evan.c.vandenberg@gmail.com>
2015-02-10 20:56:14 -0800

    Initial Commit

---
+++ ImpulseCustomCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImpulseCustomCamera
+//
+//  Created by Evan Vandenberg on 2/10/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
